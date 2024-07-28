5d91e4b4f47a0e8605502745e61e1783c6a11d84
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2024-07-28 15:17:24 +0300

    pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""
-generates a list representing pascal's triangle based on an input integer depth
+"""Pascal's triangle.
+
+Generates a list representing pascal's triangle.
+based on an input integer as depth.
 """
 
 
 def pascal_triangle(n):
-    """
-    generate pascal's triangle depth 'n'
-    """
+    """Generate pascal's triangle depth 'n'."""
     if n <= 0:
         return []
     triangle = [[1]]
     if n == 1:
         return triangle
     for i in range(n - 1):
-        triangle.append([1] + [triangle[-1][j] + triangle[-1][j + 1] for j in range(i)] + [1])
+        triangle.append([1] + [triangle[-1][j] + triangle[-1][j + 1]
+                               for j in range(i)] + [1])
     return triangle
