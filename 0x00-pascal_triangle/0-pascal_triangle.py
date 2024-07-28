2dd75c30a42f17b4f0279f54a91315c0b8e29fb1
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2024-07-28 14:53:27 +0300

    with for loop

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,13 +8,13 @@ def pascal_triangle(n):
     """
     generate pascal's triangle depth 'n'
     """
+
     i = 0
     if n <= 0:
         return []
     triangle = [[1]]
     if n == 1:
         return triangle
-    while i < n - 1:
+    for i in range(n - 1):
         triangle.append([1] + [triangle[-1][j] + triangle[-1][j + 1] for j in range(i)] + [1])
-        i += 1
     return triangle
