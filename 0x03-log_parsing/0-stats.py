009760f121c692481d57e14fee44f24a5d623d26
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2024-08-16 05:54:37 +0300

    restart

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,86 +1,43 @@
 #!/usr/bin/python3
-"""Log parsing.
-
-parsing and processing data streams in real-time. 
-This project involves reading from standard input (stdin),
-handling data in a specific format, and performing calculations
-based on the input data.
-"""
-import re
-import signal
-from sys import stdin
-from typing import Dict
+"""Log Parser"""
+import sys
 
 
-status: Dict[int, int] = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0,
-                          404: 0, 405: 0, 500: 0}
-total: int = 0
+if __name__ == '__main__':
+    file_size = [0]
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
 
+    def print_stats():
+        """ Print statistics """
+        print('File size: {}'.format(file_size[0]))
+        for key in sorted(status_codes.keys()):
+            if status_codes[key]:
+                print('{}: {}'.format(key, status_codes[key]))
 
-def verfy_format(log_line: str) -> bool:
-    """Check for a required string format.
+    def parse_line(line):
+        """ Checks the line for matches """
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Status code comes before file size
+            status_code = int(word[-2])
+            # Move through dictionary of status codes
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
 
-    Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" 
-    <status code> <file size>
-
-    Returns:
-        if log_line is correct format :True else :False
-    """
-    parts = log_line.split(' "GET /projects/260 HTTP/1.1" ')
-    log_format: str = [r"^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3} - \[.*?\]$",
-                       r"^(\d{333} (\d+|-))$"]
-    return bool(len(parts) == 2 and
-                re.match(log_format[0], parts[0]) and
-                re.match(log_format[1], parts[1]))
-
-
-def stop() -> None:
-    """Stop parsing."""
-    print_s()
-    raise KeyboardInterrupt
-
-
-def print_s() -> None:
-    """Print statistics.
-
-    global variables used:
-        status: Dict
-            {code: count, ...}
-            possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-        total: int
-            total file size
-    """
-    print(f'File size: {total}')
-    for code, count in status.items():
-        if count > 0:
-            print(f'{code}: {count}')
-
-
-def main() -> None:
-    """Main stdin loop."""
-    global status
-    global total
-    lines: int = 0
-    signal.signal(signal.SIGINT, stop)
-    for line in stdin:
-        lines = (lines + 1) % 4
-        if verfy_format(line):
-            fields = re.split('HTTP/1.1" ', line)
-            key, size = fields[1].split()
-            try:
-                key: int = int(key)
-                size: int = int(size)
-                status[key] += 1
-                total += size
-            except ValueError:
-                pass
-        if lines == 0:
-            print_s()
-            status = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0,
-                      404: 0, 405: 0, 500: 0}
-
-
-if __name__ == "__main__":
-    """RUN."""
-    main()
-
+    linenum = 1
+    try:
+        for line in sys.stdin:
+            parse_line(line)
+            """ print after every 10 lines """
+            if linenum % 10 == 0:
+                print_stats()
+            linenum += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
