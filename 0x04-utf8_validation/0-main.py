26d902297438192b2e4a4bdb305e0d39f006d803
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2024-08-23 01:07:29 +0300

    more test

--- 0x04-utf8_validation/0-main.py
+++ 0x04-utf8_validation/0-main.py
@@ -5,19 +5,22 @@ Main file for testing
 
 validUTF8 = __import__('0-validate_utf8').validUTF8
 
-data = [65]
-print(validUTF8(data))
-
-data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
-print(validUTF8(data))
-
-data = [229, 65, 127, 256]
-print(validUTF8(data))
-
 def p_v(l):
     print("...")
     print([bin(A) for A in l])
     print(validUTF8(l))
+
+
+data = [65]
+p_v(data)
+
+data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+p_v(data)
+
+data = [229, 65, 127, 256]
+p_v(data)
+
+p_v([0,1])
 
 p_v([
 0b01001000,
