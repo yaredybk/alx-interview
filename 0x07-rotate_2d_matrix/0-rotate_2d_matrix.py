a2f99c3fd9f96b4c4ec914e0577fd1f0b77be615
root <yb12ybk@gmail.com>
2024-09-09 21:30:22 +0000

    fix: syntax, pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,16 +8,17 @@ def rotate_2d_matrix(matrix):
     requirements:
     Do not return anything. The matrix must be edited in-place.
     You can assume the matrix will have 2 dimensions and will not be empty."""
-    l = len(matrix)
-    r = range(l / 2)
+    size = len(matrix)
+    r = range(int(size / 2))
     # to prevent swapping middle row twice in odd matrixs
-    r2 = range((l + 1) / 2)
+    r2 = range(int((size + 1) / 2))
     # to prevent IndexError: list index out of range
-    l -= 1
+    size -= 1
+
     def swap(i, j):
         """rotate 4 elements of a matrix inplace"""
-        ii, jj = l - i, l - j
-        matrix[i][j], matrix[j][ii], matrix[ii][jj], matrix[jj][i] = 
-        matrix[jj][i], matrix[i][j], matrix[j][ii], matrix[ii][jj]
+        ii, jj = size - i, size - j
+        matrix[i][j], matrix[j][ii], matrix[ii][jj], matrix[jj][i] = \
+            matrix[jj][i], matrix[i][j], matrix[j][ii], matrix[ii][jj]
 
-    [[swap(i, j) for i in r] for j in r]
+    [[swap(i, j) for i in r] for j in r2]
